0acda73caa4e611102d089c326773dacdc12e6b4
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-08 23:22:00 +0530

    count the digits in a given number program

---
+++ while_loop_programs/count_digits.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+count = 0
+temp = abs(num)   # handle negative numbers
+
+if temp == 0:
+    count = 1
+else:
+    while temp > 0:
+        count += 1
+        temp //= 10
+
+print("Number of digits:", count)

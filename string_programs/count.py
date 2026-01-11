58dff0c3de8ffa0d7e8d84288bfacf218906d6f8
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-11 23:53:53 +0530

    to count the vowels consonants digits and spaces

---
+++ string_programs/count.py
@@ -0,0 +1,17 @@
+s = "Hello 123"
+vowels = consonants = digits = spaces = 0
+
+for ch in s:
+    if ch.lower() in "aeiou":
+        vowels += 1
+    elif ch.isdigit():
+        digits += 1
+    elif ch == " ":
+        spaces += 1
+    elif ch.isalpha():
+        consonants += 1
+
+print("Vowels:", vowels)
+print("Consonants:", consonants)
+print("Digits:", digits)
+print("Spaces:", spaces)

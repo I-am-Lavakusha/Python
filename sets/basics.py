5d0b35685b157b51a363043c5b14d00de0b5596f
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-15 22:45:35 +0530

    basics about sets

---
+++ sets/basics.py
@@ -0,0 +1,13 @@
+#sets in python
+#sets are unordered
+#sets are written in curly brackets
+#it will not allow duplicates.
+
+#list              #tuple                #set
+#ordered            ordered              unordered
+#mutable            immutable            mutable
+#duplicates         duplicates           not allowed
+#indexing           indexing             no indexing
+
+list1=[1,2,2,3,4,4,5]
+print(set(list1))

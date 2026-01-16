5675047bb6c10c2880ae9f568662c7a8bb51ef61
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-16 23:11:56 +0530

    basics of tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,19 @@
+#tuple
+#ordered
+#immutable
+#allows duplicates.
+
+tuple1=("apple", "bananna", "cherry", "apple")
+print(tuple1)
+list1=list(tuple1)
+list1[1]="strawberry"
+tuple1=tuple(list1)
+print(tuple1)
+
+#lists mutable                #immutable
+#slower                       #faster
+#consumes more memory         # less memory
+#so many methods for list     #less methods
+#list used more               #less used
+
+

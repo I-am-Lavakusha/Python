c7976b6033703c85beab0ec1a7315fc3a1bf72e6
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-18 22:40:45 +0530

    updated the program

--- string_programs/palindrome.py
+++ string_programs/palindrome.py
@@ -1,3 +1,7 @@
+#this is the program that will check whether the program is palindrome or not
+#it uses the concept of slicing
+#it can be done by using the two pointer approach also
+
 s = "madam"
 if s == s[::-1]:
     print("Palindrome")
